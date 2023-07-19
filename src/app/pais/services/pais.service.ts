911bb69b7c12df8cd981a91f0e29c50f9ef8cb3a
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-18 21:52:35 -0500

    refactor: change functions names to english
    added prettier to pais service file
    
    renamed functions are not called in components but the old names

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -4,48 +4,39 @@ import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class PaisService {
+  private apiUrl: string = 'https://restcountries.com/v3.1/';
 
-  private apiUrl: string = "https://restcountries.com/v3.1/";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  buscarPais(termino: string) {
-    const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      );
+  searchCountry(term: string) {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 
-  buscarCapital(termino: string) {
-    const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      );
+  searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 
-  buscarRegion(termino: string) {
-    const url = `${this.apiUrl}/region/${termino}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      );
+  searchRegion(term: string) {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 
-  verPais(paisCodigo: string): Observable<Country[] | null> {
-    const url = `${this.apiUrl}/alpha/${paisCodigo}`;
-    return this.http.get<Country[]>(url)
-      // .pipe(
-      //   map( paises => paises.length > 0 ? paises[0] : null),
-      //   catchError(error => of([]))
-      // );
-      .pipe(
-        catchError(error => of([]))
-      )
+  viewCountry(countryCode: string): Observable<Country[] | null> {
+    const url = `${this.apiUrl}/alpha/${countryCode}`;
+    return (
+      this.http
+        .get<Country[]>(url)
+        // .pipe(
+        //   map( paises => paises.length > 0 ? paises[0] : null),
+        //   catchError(error => of([]))
+        // );
+        .pipe(catchError((error) => of([])))
+    );
   }
 }
-
